ff36d75c0dd7c4b3c13672a23346a1d9adbd1fc2
ReiAyanami24 <44541791+maradona4@users.noreply.github.com>
2024-11-20 16:36:48 -0300

    Update index.js
    Erro corrigido:
    TypeError: Cannot destructure property 'message' of 'result' as it is null.
    Fora do escopo: Mensagem não encontrada!
    
    Alterações feitas:
    Adicionado: const TypeFound e tipos.
    Agora, senderKeyDistributionMessage e messageContextInfo serão ignorados.

--- index.js
+++ index.js
@@ -68,8 +68,6 @@ async function deleteRun(kill = envInfo.functions.exec.arguments.kill.value, dat
                 time,
                 mimetype,
             } = data;
-            if (type === 'messageContextInfo') return; // Se for 'messageContextInfo', será ignorado porque as mensagens não estão fora do tipo
-
             const isMediaMessage = quoteThis?.message?.stickerMessage || quoteThis?.message?.imageMessage || quoteThis?.message?.videoMessage || quoteThis?.message?.audioMessage || quoteThis?.message?.documentWithCaptionMessage?.message?.documentMessage || quoteThis?.message?.documentMessage;
             const isVisu = quoteThis.message?.viewOnceMessageV2?.message?.videoMessage || quoteThis.message?.viewOnceMessage?.message?.videoMessage || quoteThis?.message?.viewOnceMessageV2?.message?.imageMessage || quoteThis.message?.viewOnceMessage?.message?.imageMessage || false;
             if (isVisu) {
@@ -81,6 +79,11 @@ async function deleteRun(kill = envInfo.functions.exec.arguments.kill.value, dat
             const userNames = Indexer('sql').get('personal', mentionUser, chatId).value;
             const checkName = userNames === 'default' || !userNames.name.text.trim() ? pushname : userNames.name.text.trim();
             const Msg = quoteThis?.message?.documentWithCaptionMessage?.message?.documentMessage?.caption || quoteThis?.message?.documentMessage?.caption || quoteThis?.message?.videoMessage?.caption || quoteThis?.message?.imageMessage?.caption || quoteThis?.message?.extendedTextMessage?.text || quoteThis.message?.conversation || '';
+            const TypeFound = quoteThis?.message;
+
+            // Verifica se a mensagem contém 'senderKeyDistributionMessage' ou 'messageContextInfo'
+            const tipos = (quoteThis?.message?.senderKeyDistributionMessage || quoteThis?.message?.messageContextInfo) ? (TypeFound === 'conversation' ? 'conversation' : (TypeFound === 'extendedTextMessage' ? 'extendedTextMessage' : 'conversation')) : type; // Caso contrário, usa o valor original de 'type'
+
             const editarID = quoteThis?.message?.editedMessage?.message?.protocolMessage?.key?.id || quoteThis?.message?.protocolMessage?.key?.id || '';
             const FileNameDoc = quoteThis?.message?.documentWithCaptionMessage?.message?.documentMessage?.fileName || quoteThis?.message?.documentMessage?.fileName || false;
             const contactCard = quoteThis.message?.contactMessage?.vcard ? quoteThis.message.contactMessage.vcard : false;
@@ -92,7 +95,7 @@ async function deleteRun(kill = envInfo.functions.exec.arguments.kill.value, dat
             let result;
             let tiposValidosRegExp = /^(application\/(pdf|msword|vnd\.openxmlformats-officedocument\.wordprocessingml\.document|vnd\.ms-excel|vnd\.openxmlformats-officedocument\.spreadsheetml\.sheet|zip|x-rar-compressed|x-7z-compressed|x-tar|gzip|vnd\.oasis\.opendocument\.(text|spreadsheet|presentation)|epub\+zip|json|rtf|octet-stream)|text\/(plain|csv|markdown|html)|application\/javascript)$/;
 
-            switch (type) {
+            switch (tipos) {
             // Anti editado
             case 'editedMessage': {
                 const editedMessageObj = quoteThis?.editedMessage?.message?.protocolMessage?.editedMessage || quoteThis?.message?.editedMessage?.message?.protocolMessage?.editedMessage || quoteThis?.editedMessage?.message?.editedMessage;
@@ -125,51 +128,52 @@ async function deleteRun(kill = envInfo.functions.exec.arguments.kill.value, dat
             // Anti Deletado
             case 'protocolMessage': {
                 result = await checkDeletedMessage(user, editarID);
-                // eslint-disable-next-line no-unused-vars
-                const { message, captionMessage, oldBody, tipos, upload, status, doctitle } = result;
-                if (tipos === 'video/mp4') {
-                    baileysMessage.video = upload;
-                    baileysMessage.caption = `*⚠️ ALERTA DO VÍDEO 🎬*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ VÍDEO FOI APAGADO E DETECTADO*\n> ${message || captionMessage || ''}`;
-                    baileysMessage.mimetype = tipos;
-                } else if (tipos === 'image/jpeg') {
-                    baileysMessage.viewOnce = false;
-                    baileysMessage.image = upload;
-                    baileysMessage.caption = `*⚠️ ALERTA DA IMAGEM 🖼*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ IMAGEM FOI APAGADA E DETECTADA*\n> ${message || captionMessage || ''}`;
-                    baileysMessage.mimetype = tipos;
-                } else if (tipos === 'image/webp') {
-                    await kill.sendMessage(monitorID, { sticker: upload });
-                    await Indexer('others').sleep(1000); // Intervalo de 1 segundo entre stickers, risco de banimento por duplicação no WhatsAp
-                    baileysMessage.text = `*⚠️ ALERTA DAS FIGURINHAS 👾*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ FOI APAGADA E DETECTADA*\n> ${message || captionMessage || ''}`;
-                } else if (tipos === 'audio/mp4') {
-                    await kill.sendMessage(monitorID, { audio: upload, mimetype: tipos, ptt: false });
-                    await Indexer('others').sleep(1000); // Intervalo de 1 segundo entre audio, risco de banimento por duplicação no WhatsAp
-                    baileysMessage.text = `*⚠️ ALERTA DO ÁUDIO 🔊*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ ÁUDIO FOI APAGADO E DETECTADO*\n> ${message || captionMessage || ''}`;
-                } else if (tipos === 'vCard') {
+                if (result) {
+                    const { message, captionMessage, oldBody, tipos, upload, status, doctitle } = result;
+                    if (tipos === 'video/mp4') {
+                        baileysMessage.video = upload;
+                        baileysMessage.caption = `*⚠️ ALERTA DO VÍDEO 🎬*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ VÍDEO FOI APAGADO E DETECTADO*\n> ${message || captionMessage || ''}`;
+                        baileysMessage.mimetype = tipos;
+                    } else if (tipos === 'image/jpeg') {
+                        baileysMessage.viewOnce = false;
+                        baileysMessage.image = upload;
+                        baileysMessage.caption = `*⚠️ ALERTA DA IMAGEM 🖼*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ IMAGEM FOI APAGADA E DETECTADA*\n> ${message || captionMessage || ''}`;
+                        baileysMessage.mimetype = tipos;
+                    } else if (tipos === 'image/webp') {
+                        await kill.sendMessage(monitorID, { sticker: upload });
+                        await Indexer('others').sleep(1000); // Intervalo de 1 segundo entre stickers, risco de banimento por duplicação no WhatsAp
+                        baileysMessage.text = `*⚠️ ALERTA DAS FIGURINHAS 👾*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ FOI APAGADA E DETECTADA*\n> ${message || captionMessage || ''}`;
+                    } else if (tipos === 'audio/mp4') {
+                        await kill.sendMessage(monitorID, { audio: upload, mimetype: tipos, ptt: false });
+                        await Indexer('others').sleep(1000); // Intervalo de 1 segundo entre audio, risco de banimento por duplicação no WhatsAp
+                        baileysMessage.text = `*⚠️ ALERTA DO ÁUDIO 🔊*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ ÁUDIO FOI APAGADO E DETECTADO*\n> ${message || captionMessage || ''}`;
+                    } else if (tipos === 'vCard') {
                     // Extração do nome a partir da vCard
-                    const displayNameMatch = upload.match(/FN:(.*)/); // RegEx para capturar o valor após 'FN:'
-                    const displayName = displayNameMatch ? displayNameMatch[1].trim() : 'Desconhecido';
+                        const displayNameMatch = upload.match(/FN:(.*)/); // RegEx para capturar o valor após 'FN:'
+                        const displayName = displayNameMatch ? displayNameMatch[1].trim() : 'Desconhecido';
 
-                    // Extração do número de telefone da vCard
-                    const phoneMatch = upload.match(/TEL;waid=(\d+):(\+?\d+)/); // RegEx para capturar o número de telefone com WhatsApp
-                    const phoneNumber = phoneMatch ? `${phoneMatch[1]}:${phoneMatch[2]}` : 'Número não encontrado';
-                    const vcard = 'BEGIN:VCARD\n'
+                        // Extração do número de telefone da vCard
+                        const phoneMatch = upload.match(/TEL;waid=(\d+):(\+?\d+)/); // RegEx para capturar o número de telefone com WhatsApp
+                        const phoneNumber = phoneMatch ? `${phoneMatch[1]}:${phoneMatch[2]}` : 'Número não encontrado';
+                        const vcard = 'BEGIN:VCARD\n'
                         + 'VERSION:3.0\n'
                         + `FN:${displayName}\n`
                         + `ORG:${displayName};\n`
                         + `TEL;type=CELL;type=VOICE;waid=${phoneNumber}\n`
                         + 'END:VCARD';
-                    await kill.sendMessage(monitorID, { contacts: { displayName: displayName, contacts: [{ vcard }] } });
-                    await Indexer('others').sleep(1000); // Intervalo de 1 segundo entre audio, risco de banimento por duplicação no WhatsAp
-                    baileysMessage.text = `*⚠️ ALERTA DO CONTATO 🔊*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ CONTATO FOI APAGADO E DETECTADO*\n> ${message || captionMessage || ''}`;
-                } else if (tiposValidosRegExp.test(tipos)) {
-                    baileysMessage.document = upload;
-                    baileysMessage.mimetype = tipos;
-                    baileysMessage.fileName = doctitle;
-                    baileysMessage.caption = `*⚠️ ALERTA DO DOCUMENTOS 📚*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ DOCUMENTOS FORAM APAGADOS E DETECTADOS*\n> ${message || captionMessage || ''}`;
-                } else {
-                    baileysMessage.text = `*⚠️ ALERTA DAS MENSAGENS 📝*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ MENSAGENS FORAM APAGADAS E DETECTADAS*\n> ${message || captionMessage || ''}`;
+                        await kill.sendMessage(monitorID, { contacts: { displayName: displayName, contacts: [{ vcard }] } });
+                        await Indexer('others').sleep(1000); // Intervalo de 1 segundo entre audio, risco de banimento por duplicação no WhatsAp
+                        baileysMessage.text = `*⚠️ ALERTA DO CONTATO 🔊*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ CONTATO FOI APAGADO E DETECTADO*\n> ${message || captionMessage || ''}`;
+                    } else if (tiposValidosRegExp.test(tipos)) {
+                        baileysMessage.document = upload;
+                        baileysMessage.mimetype = tipos;
+                        baileysMessage.fileName = doctitle;
+                        baileysMessage.caption = `*⚠️ ALERTA DO DOCUMENTOS 📚*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ DOCUMENTOS FORAM APAGADOS E DETECTADOS*\n> ${message || captionMessage || ''}`;
+                    } else {
+                        baileysMessage.text = `*⚠️ ALERTA DAS MENSAGENS 📝*\n*✪ PL:* ${checkName}\n*✪ GP:* ${name}\n*✪ DDD:* ${user.replace('@s.whatsapp.net', '')}\n*✪ TEMPO:* ${time}\n*✪ MENSAGENS FORAM APAGADAS E DETECTADAS*\n> ${message || captionMessage || ''}`;
+                    }
+                    alertaLog = true;
                 }
-                alertaLog = true;
                 break;
             }
 
